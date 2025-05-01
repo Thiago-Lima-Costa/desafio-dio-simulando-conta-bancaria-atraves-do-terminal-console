cad633505fb2a685f135078ca382922b32bdfb6b
Thiago Costa <lima.costa.dev@gmail.com>
2025-05-01 20:36:40 -0300

    Update ContaTerminal.java

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -7,18 +7,18 @@ public class ContaTerminal {
 		Scanner entrada = new Scanner(System.in);
 		
 		System.out.println("Por favor, digite o Nome do Cliente:");
-        String nomeCliente = entrada.nextLine();
+        	String nomeCliente = entrada.nextLine();
 		
 		System.out.println("Por favor, digite o número da Agência:");
-        String agencia = entrada.nextLine();
+        	String agencia = entrada.nextLine();
 
-        System.out.println("Por favor, digite o número da Conta:");
-        int numero = Integer.parseInt(entrada.nextLine());
+        	System.out.println("Por favor, digite o número da Conta:");
+        	int numero = Integer.parseInt(entrada.nextLine());
 
-        System.out.println("Por favor, digite o Saldo:");
-        double saldo = Double.parseDouble(entrada.nextLine());
+        	System.out.println("Por favor, digite o Saldo:");
+        	double saldo = Double.parseDouble(entrada.nextLine());
 
-        String mensagem = "Olá ".concat(nomeCliente)
+        	String mensagem = "Olá ".concat(nomeCliente)
                          .concat(", obrigado por criar uma conta em nosso banco, sua agência é ")
                          .concat(agencia)
                          .concat(", conta ")
@@ -27,10 +27,10 @@ public class ContaTerminal {
                          .concat(String.valueOf(saldo))
                          .concat(" já está disponível para saque");
 
-        System.out.println(mensagem);
+        	System.out.println(mensagem);
 		
 		
-        entrada.close();
+        	entrada.close();
 	}
 
 }
